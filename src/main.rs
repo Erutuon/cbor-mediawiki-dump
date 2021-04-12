4c83151f4a8a5eedbdb2eac9250e26566f468ff2
Erutuon <5840197+Erutuon@users.noreply.github.com>
2021-04-11 22:09:05 -0500

    refactor if expression into match

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{convert::TryFrom, io::BufReader, fs::File, path::PathBuf};
+use std::{convert::TryFrom, fs::File, io::BufReader, path::PathBuf};
 
 use bzip2::read::BzDecoder;
 use lzma::reader::LzmaReader;
@@ -8,13 +8,13 @@ fn main() -> anyhow::Result<()> {
     let pages_xml_path = args
         .opt_value_from_os_str(["-f", "--file"], |p| PathBuf::try_from(p))?
         .unwrap_or_else(|| "pages-articles.xml".into());
-    let file =File::open(&pages_xml_path)?;
-    if pages_xml_path.extension() == Some("bz2".as_ref()) {
-        cbor_mediawiki_dump::parse(BufReader::new(BzDecoder::new(file)))?;
-    } else if pages_xml_path.extension() == Some("7z".as_ref()) {
-        cbor_mediawiki_dump::parse(BufReader::new(LzmaReader::new_decompressor(file)?))?;
-    } else {
-        cbor_mediawiki_dump::parse(BufReader::new(file))?;
+    let file = File::open(&pages_xml_path)?;
+    match pages_xml_path.extension().and_then(|s| s.to_str()) {
+        Some("bz2") => cbor_mediawiki_dump::parse(BufReader::new(BzDecoder::new(file)))?,
+        Some("7z") => {
+            cbor_mediawiki_dump::parse(BufReader::new(LzmaReader::new_decompressor(file)?))?
+        }
+        _ => cbor_mediawiki_dump::parse(BufReader::new(file))?,
     }
     Ok(())
 }
