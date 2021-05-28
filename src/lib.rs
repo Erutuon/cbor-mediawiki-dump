092c74207cb495479bca7666d021aab5e3fc59e6
Erutuon <5840197+Erutuon@users.noreply.github.com>
2021-05-28 15:02:28 -0500

    add `find_page` and processing with closure, put decompression in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,83 @@
-use std::{borrow::Cow, io::BufRead, net::IpAddr};
+use std::{
+    borrow::Cow,
+    convert::Infallible,
+    fs::File,
+    io::{BufRead, BufReader},
+    net::IpAddr,
+    path::{Path, PathBuf},
+};
 
+use bzip2::read::BzDecoder;
 use chrono::{DateTime, Utc};
-use quick_xml::{Reader, events::{BytesStart, Event}};
+use lzma::{LzmaError, LzmaReader};
+use memchr::memmem;
+use quick_xml::{
+    events::{BytesStart, Event},
+    Reader,
+};
 use serde::Serialize;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum Error {
+pub enum Error<E: std::error::Error = Infallible> {
     #[error("invalid XML (schema or format) at position {position}")]
     Format { position: usize },
     #[error("failed to unescape or decode UTF-8 at position {position}")]
     FailedToDecode { position: usize },
     #[error("failed to open XML file: {0}")]
     File(quick_xml::Error),
+    #[error("Done deserializing")]
+    ShortCircuit,
+    #[error("Failed to {action} at {}", path.display())]
+    Io {
+        action: &'static str,
+        source: std::io::Error,
+        path: PathBuf,
+    },
+    #[error("Failed to decode LZMA at {}", path.display())]
+    Lzma { source: LzmaError, path: PathBuf },
+    #[error("{0}")]
+    Other(E),
 }
 
-impl Error {
+impl<E: std::error::Error> Error<E> {
     fn format<R: BufRead>(reader: &Reader<R>) -> Self {
         Self::Format {
             position: reader.buffer_position(),
+        }
+    }
+
+    fn from_io<P: Into<PathBuf>>(action: &'static str, source: std::io::Error, path: P) -> Self {
+        Error::Io {
+            action,
+            source,
+            path: path.into(),
         }
     }
 }
 
 #[derive(Serialize)]
 pub struct Page {
-    title: String,
-    namespace: i32,
-    id: u32,
-    redirect_target: Option<String>,
-    restrictions: Option<String>,
-    revisions: Vec<Revision>,
+    pub title: String,
+    pub namespace: i32,
+    pub id: u32,
+    pub redirect_target: Option<String>,
+    pub restrictions: Option<String>,
+    pub revisions: Vec<Revision>,
 }
 
 #[derive(Serialize)]
 pub struct Revision {
-    id: u32,
-    parent_id: Option<u32>,
-    timestamp: DateTime<Utc>,
-    contributor: Contributor,
-    minor: bool,
-    comment: Comment,
-    model: String,  // Could be converted to integer using hashmap.
-    format: String, // Could be converted to integer using hashmap.
-    text: String,
-    sha1: String,
+    pub id: u32,
+    pub parent_id: Option<u32>,
+    pub timestamp: DateTime<Utc>,
+    pub contributor: Contributor,
+    pub minor: bool,
+    pub comment: Comment,
+    pub model: String,  // Could be converted to integer using hashmap.
+    pub format: String, // Could be converted to integer using hashmap.
+    pub text: String,
+    pub sha1: String,
 }
 
 #[derive(Serialize)]
@@ -62,10 +95,10 @@ pub enum Contributor {
     User { username: String, id: u32 },
 }
 
-pub fn get_start_tag<'a, R: BufRead>(
+pub fn get_start_tag<'a, R: BufRead, E: std::error::Error>(
     reader: &mut Reader<R>,
     buf: &'a mut Vec<u8>,
-) -> Result<(BytesStart<'a>, bool), Error> {
+) -> Result<(BytesStart<'a>, bool), Error<E>> {
     match reader.read_event(buf) {
         Ok(Event::Start(start)) => Ok((start, false)),
         Ok(Event::Empty(start)) => Ok((start, true)),
@@ -73,7 +106,11 @@ pub fn get_start_tag<'a, R: BufRead>(
     }
 }
 
-pub fn expect_tag_start<R: BufRead>(tag: &str, reader: &mut Reader<R>, buf: &mut Vec<u8>) -> Result<(), Error> {
+pub fn expect_tag_start<R: BufRead, E: std::error::Error>(
+    tag: &str,
+    reader: &mut Reader<R>,
+    buf: &mut Vec<u8>,
+) -> Result<(), Error<E>> {
     let start = reader.read_event(buf).map_err(|_| Error::format(reader))?;
     // dbg!(&start);
     if matches!(start, Event::Start(start) if start.name() == tag.as_bytes()) {
@@ -83,7 +120,11 @@ pub fn expect_tag_start<R: BufRead>(tag: &str, reader: &mut Reader<R>, buf: &mut
     }
 }
 
-pub fn expect_tag_end<R: BufRead>(tag: &str, reader: &mut Reader<R>, buf: &mut Vec<u8>) -> Result<(), Error> {
+pub fn expect_tag_end<R: BufRead, E: std::error::Error>(
+    tag: &str,
+    reader: &mut Reader<R>,
+    buf: &mut Vec<u8>,
+) -> Result<(), Error<E>> {
     let end = reader.read_event(buf).map_err(|_| Error::format(reader))?;
     // dbg!(&end);
     if matches!(end, Event::End(end) if end.name() == tag.as_bytes()) {
@@ -93,7 +134,10 @@ pub fn expect_tag_end<R: BufRead>(tag: &str, reader: &mut Reader<R>, buf: &mut V
     }
 }
 
-pub fn skip_text<R: BufRead>(reader: &mut Reader<R>, buf: &mut Vec<u8>) -> Result<(), Error> {
+pub fn skip_text<R: BufRead, E: std::error::Error>(
+    reader: &mut Reader<R>,
+    buf: &mut Vec<u8>,
+) -> Result<(), Error<E>> {
     let text = reader.read_event(buf).map_err(|_| Error::format(reader))?;
     // dbg!(&text);
     if matches!(text, Event::Text(_)) {
@@ -103,12 +147,17 @@ pub fn skip_text<R: BufRead>(reader: &mut Reader<R>, buf: &mut Vec<u8>) -> Resul
     }
 }
 
-pub fn map_unescaped_text<R: BufRead, F: FnMut(Cow<'_, [u8]>) -> Result<T, Error>, T>(
+pub fn map_unescaped_text<
+    R: BufRead,
+    T,
+    E: std::error::Error,
+    F: FnMut(Cow<'_, [u8]>) -> Result<T, Error<E>>,
+>(
     reader: &mut Reader<R>,
     buf: &mut Vec<u8>,
     tag: &str,
     mut f: F,
-) -> Result<T, Error> {
+) -> Result<T, Error<E>> {
     expect_tag_start(tag, reader, buf)?;
     match reader.read_event(buf).map_err(|_| Error::format(reader))? {
         Event::Text(text) => {
@@ -127,23 +176,59 @@ pub fn map_unescaped_text<R: BufRead, F: FnMut(Cow<'_, [u8]>) -> Result<T, Error
     }
 }
 
-pub fn parse<R: BufRead>(reader: R) -> Result<(), Error> {
+// Search for <page> containing <title> with given title.
+pub fn find_page(title_to_find: &str, xml: &[u8]) -> Result<Option<Page>, Error<Infallible>> {
+    let title_tag = format!(
+        "<title>{}</title>",
+        title_to_find
+            .replace('&', "&amp;")
+            .replace('"', "&quot;")
+            .replace('<', "&lt;")
+            .replace('>', "&gt")
+    );
+    if let Some(title_tag_index) = memmem::find(xml, title_tag.as_ref()) {
+        if let Some(page_tag_index) = memmem::rfind(&xml[..title_tag_index], b"<page>") {
+            let mut found_page = None;
+            parse(
+                &xml[page_tag_index..],
+                |page| {
+                    if page.title == title_to_find {
+                        found_page = Some(page);
+                    }
+                    Err(Error::ShortCircuit)
+                },
+                false,
+            )?;
+            return Ok(found_page);
+        }
+    }
+    Ok(None)
+}
+
+pub fn parse<R: BufRead, F: FnMut(Page) -> Result<(), Error<E>>, E: std::error::Error>(
+    reader: R,
+    mut page_processor: F,
+    skip_header: bool,
+) -> Result<(), Error<E>> {
     // Bigger than maximum revision length (2 MiB).
     let mut buf = Vec::with_capacity(3 * 1024 * 1024);
     let mut reader = Reader::from_reader(reader);
 
     skip_text(&mut reader, &mut buf)?;
-    expect_tag_start("mediawiki", &mut reader, &mut buf)?;
-    skip_text(&mut reader, &mut buf)?;
-    expect_tag_start("siteinfo", &mut reader, &mut buf)?;
-    reader
-        .read_to_end("siteinfo", &mut buf)
-        .map_err(|_| Error::format(&reader))?;
-    skip_text(&mut reader, &mut buf)?;
+    // Skip over initial mediawiki tag.
+    if skip_header {
+        expect_tag_start("mediawiki", &mut reader, &mut buf)?;
+        skip_text(&mut reader, &mut buf)?;
+        expect_tag_start("siteinfo", &mut reader, &mut buf)?;
+        reader
+            .read_to_end("siteinfo", &mut buf)
+            .map_err(|_| Error::format(&reader))?;
+        skip_text(&mut reader, &mut buf)?;
+    }
     buf.clear();
 
-    let stdout = std::io::stdout();
-    let mut stdout = stdout.lock();
+    // let stdout = std::io::stdout();
+    // let mut stdout = stdout.lock();
 
     let mut restrictions_buffer = Vec::new();
 
@@ -295,14 +380,14 @@ pub fn parse<R: BufRead>(reader: R) -> Result<(), Error> {
                     }
                 } else {
                     skip_text(&mut reader, &mut buf)?;
-    
+
                     let (tag, _) = get_start_tag(&mut reader, &mut buf)?;
                     let contributor = if tag.name() == b"username" {
                         let username = reader
                             .read_text("username", &mut buf)
                             .map_err(|_| Error::format(&reader))?;
                         skip_text(&mut reader, &mut buf)?;
-    
+
                         let position = reader.buffer_position();
                         let id: u32 = map_unescaped_text(&mut reader, &mut buf, "id", |id| {
                             std::str::from_utf8(id.as_ref())
@@ -322,9 +407,9 @@ pub fn parse<R: BufRead>(reader: R) -> Result<(), Error> {
                     } else {
                         return Err(Error::format(&reader));
                     };
-    
+
                     expect_tag_end("contributor", &mut reader, &mut buf)?;
-    
+
                     contributor
                 }
             };
@@ -429,7 +514,7 @@ pub fn parse<R: BufRead>(reader: R) -> Result<(), Error> {
             expect_tag_end("revision", &mut reader, &mut buf)?;
             skip_text(&mut reader, &mut buf)?;
 
-            let revision = Revision {
+            revisions.push(Revision {
                 id,
                 parent_id,
                 timestamp,
@@ -440,21 +525,55 @@ pub fn parse<R: BufRead>(reader: R) -> Result<(), Error> {
                 format,
                 text,
                 sha1,
-            };
-            revisions.push(revision);
+            });
         }
 
-        let page = Page {
+        match page_processor(Page {
             title,
             namespace,
             id,
             redirect_target,
             restrictions,
             revisions,
-        };
-
-        serde_cbor::to_writer(&mut stdout, &page).expect("failed to write CBOR to stdout");
+        }) {
+            Err(Error::ShortCircuit) => return Ok(()),
+            Err(e) => return Err(e),
+            _ => {}
+        }
+        // serde_cbor::to_writer(&mut stdout, &page).expect("failed to write CBOR to stdout");
 
         restrictions_buffer.clear();
+    }
+}
+
+pub fn parse_from_file<
+    P: AsRef<Path>,
+    F: FnMut(Page) -> Result<(), Error<E>>,
+    E: std::error::Error,
+>(
+    path: P,
+    page_processor: F,
+    skip_header: bool,
+) -> Result<(), Error<E>> {
+    let path = path.as_ref();
+    let file = File::open(path).map_err(|e| Error::from_io("open", e, path))?;
+
+    match path.extension().and_then(|s| s.to_str()) {
+        Some("bz2") => parse(
+            BufReader::new(BzDecoder::new(file)),
+            page_processor,
+            skip_header,
+        ),
+        Some("7z") => parse(
+            BufReader::new(
+                LzmaReader::new_decompressor(file).map_err(|source| Error::Lzma {
+                    source,
+                    path: path.into(),
+                })?,
+            ),
+            page_processor,
+            skip_header,
+        ),
+        _ => parse(BufReader::new(file), page_processor, skip_header),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use std::{convert::TryFrom, fs::File, io::BufReader, path::PathBuf};
+use std::{convert::TryFrom, path::PathBuf};
 
-use bzip2::read::BzDecoder;
-use lzma::reader::LzmaReader;
+use cbor_mediawiki_dump::{Error, parse_from_file};
 
 fn main() -> anyhow::Result<()> {
     let mut args = pico_args::Arguments::from_env();
+    #[allow(clippy::redundant_closure)]
     let pages_xml_path = args
         .opt_value_from_os_str(["-f", "--file"], |p| PathBuf::try_from(p))?
         .unwrap_or_else(|| "pages-articles.xml".into());
-    let file = File::open(&pages_xml_path)?;
-    match pages_xml_path.extension().and_then(|s| s.to_str()) {
-        Some("bz2") => cbor_mediawiki_dump::parse(BufReader::new(BzDecoder::new(file)))?,
-        Some("7z") => {
-            cbor_mediawiki_dump::parse(BufReader::new(LzmaReader::new_decompressor(file)?))?
-        }
-        _ => cbor_mediawiki_dump::parse(BufReader::new(file))?,
-    }
+
+    let stdout = std::io::stdout();
+    let mut stdout = stdout.lock();
+
+    parse_from_file(
+        pages_xml_path,
+        |page| serde_cbor::to_writer(&mut stdout, &page).map_err(Error::Other),
+        true,
+    )?;
+
     Ok(())
 }
